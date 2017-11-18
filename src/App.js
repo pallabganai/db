d4b1daf4a5b586828097523d2c7c69d704582d42
Pallab Kumar Ganai <pallabganai@users.noreply.github.com>
2017-11-19 00:15:58 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.<br>
+          This is an example of auto deployment.
         </p>
       </div>
     );
